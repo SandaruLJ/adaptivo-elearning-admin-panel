8f7c57d6237db3342589519319eaf91d6b41f620
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2022-03-22 10:24:18 +0530

    Resolve Merge Conflicts

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,23 +5,17 @@ import { BrowserRouter as Router, Redirect, Route, Routes } from "react-router-d
 
 import "./Main.css";
 import AddCourse from "../Add Course/AddCourse";
-<<<<<<< HEAD
 import AddQuestion from "../Add Question/AddQuestion";
 
-=======
 import AllCourse from "../All Course/AllCourse";
->>>>>>> 21a06df94ce0a8bc2eca1ebb229278c32d24acb4
 const Main = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Dashboard />}></Route>
-<<<<<<< HEAD
       <Route exact path="/courses" element={<AddCourse />}></Route>
       <Route exact path="/qna" element={<AddQuestion />}></Route>
-=======
       <Route exact path="/courses" element={<AllCourse />}></Route>
       <Route exact path="/courses/add" element={<AddCourse />}></Route>
->>>>>>> 21a06df94ce0a8bc2eca1ebb229278c32d24acb4
     </Routes>
   );
 };

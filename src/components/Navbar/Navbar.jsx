923d9d4ea216fe62db17e437e5ba0103e1a9e5bf
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2022-07-01 03:31:12 +0530

    Fix bug in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,12 +23,10 @@ const navLinks = [
         path: "/add/courses",
       },
     ],
-    state: "isCourseClicked",
-    dispatchMethod: navMenuActions.clickCourses,
   },
   { text: "Concepts", icon: <MenuBook />, path: "/concepts", auth: ["admin", "instructor"] },
   {
-    text: "Category",
+    text: "Categories",
     icon: <Category />,
     path: "/categories",
     auth: ["admin", "instructor"],
@@ -42,13 +40,41 @@ const navLinks = [
         path: "/categories/add",
       },
     ],
-    state: "isCategoryClicked",
-    dispatchMethod: navMenuActions.clickCategory,
   },
-  { text: "Instructors", icon: <Groups />, path: "/instructors", auth: ["admin", "instructor"], state: "isInstructorsClicked", dispatchMethod: navMenuActions.clickInstructors },
+  {
+    text: "Instructors",
+    icon: <Groups />,
+    path: "/instructors",
+    auth: ["admin", "instructor"],
+    subLinks: [
+      {
+        text: "All Instructors",
+        path: "/instructors",
+      },
+      {
+        text: "Add Instructors",
+        path: "/add/instructors",
+      },
+    ],
+  },
   { text: "Q & A", icon: <QuestionAnswer />, path: "/qna", auth: ["admin", "instructor"] },
-  { text: "Users", icon: <Group />, path: "/users", auth: ["admin", "instructor"], state: "isUsersClicked" },
-  { text: "Subscriptions", icon: <Subscriptions />, path: "/subscriptions", auth: ["admin", "instructor"], state: "isSubscriptionsClicked" },
+  {
+    text: "Users",
+    icon: <Group />,
+    path: "/users",
+    auth: ["admin", "instructor"],
+    subLinks: [
+      {
+        text: "All Users",
+        path: "/users",
+      },
+      {
+        text: "Add User",
+        path: "/add/user",
+      },
+    ],
+  },
+  { text: "Subscriptions", icon: <Subscriptions />, path: "/subscriptions", auth: ["admin", "instructor"] },
 ];
 const NavBar = (props) => {
   return (
@@ -69,16 +95,7 @@ const NavBar = (props) => {
         {/* Render each navigation button in the list with props */}
         {navLinks.map((navLink) => (
           // Only render NavButtons which are allowed for the user type
-          <NavButton
-            key={navLink.text}
-            text={navLink.text}
-            icon={navLink.icon}
-            path={navLink.path}
-            collapsed={props.collapsed}
-            subLinks={navLink.subLinks}
-            state={navLink.state}
-            dispatchMethod={navLink.dispatchMethod}
-          />
+          <NavButton key={navLink.text} text={navLink.text} icon={navLink.icon} path={navLink.path} collapsed={props.collapsed} subLinks={navLink.subLinks} />
         ))}
       </List>
     </div>
